0d45e7ed277107ebefb6389a62ca7f06285f16ed
Szymon Sakowicz <s.sakowicz@tidio.net>
2025-02-02 21:12:40 +0100

    Add support for tracking multiple budgets

--- src/actual-budget-prometheus-exporter.ts
+++ src/actual-budget-prometheus-exporter.ts
@@ -1,20 +1,30 @@
-import { Metrics, MetricsRendererI, StatsFetcherI } from './types';
+import {
+  Budget, Metrics, MetricsRendererI, Stats, StatsFetcherI,
+} from './types';
 
 export default class ActualBudgetPrometheusExporter {
   private metricsRenderer: MetricsRendererI;
 
   private statsFetcher: StatsFetcherI;
 
+  private budgets: Budget[];
+
   constructor(
     statsFetcher: StatsFetcherI,
     metricsRenderer: MetricsRendererI,
+    budgets: Budget[],
   ) {
     this.metricsRenderer = metricsRenderer;
     this.statsFetcher = statsFetcher;
+    this.budgets = budgets;
   }
 
   async getMetrics(): Promise<Metrics> {
-    const stats = await this.statsFetcher.fetch();
+    const stats: Stats[] = [];
+    for (const budget of this.budgets) {
+      console.log(`Fetching stats for budget: ${budget.name}`);
+      stats.push(await this.statsFetcher.fetch(budget));
+    }
     const register = this.metricsRenderer.renderFromStats(stats);
     return {
       metrics: await register.metrics(),
